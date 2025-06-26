f1aa7a9714c72411f6f22c786b0809ab6207fde3
ProfDecodor <julian@gotoandplay.be>
2025-06-26 13:33:25 +0200

    refonte en vue.js

--- scripts/background.js
+++
@@ -1,6 +0,0 @@
-//gère l'ouverture des préférences du plugin
-/*browser.runtime.onMessage.addListener(function(request) {
-    if (request.message === "open_addon_preferences") {
-        browser.tabs.create({"url":"/options.html"});
-    }
-});*/
